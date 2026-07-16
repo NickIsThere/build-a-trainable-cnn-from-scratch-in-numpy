67cf1a7a7be924e98da02ccf3324056df0f4b077
Nick Grebe <77935320+NickIsThere@users.noreply.github.com>
2026-07-16 15:35:00 +0200

    Step 10: he_std

--- model.py
+++ model.py
@@ -82,8 +82,10 @@ def accuracy(logits_or_probs, labels):
     maxs = argmax_rows(logits_or_probs)
     return np.mean(maxs == labels)
 
-# Step 10 - he_std (not yet solved)
-# TODO: implement
+# Step 10 - he_std
+def he_std(fan_in):
+    # TODO: return the He initialization standard deviation sqrt(2 / fan_in).
+    return np.sqrt(np.divide(2, fan_in))
 
 # Step 11 - he_init (not yet solved)
 # TODO: implement
